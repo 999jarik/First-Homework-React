d24407928f76938ad741f7836564d2807c5e96b9
Yaroslav <999jarik@gmail.com>
2024-03-29 00:19:28 +0200

    Done

---
+++ src/components/SecondComponent/SecondComponent.jsx
@@ -0,0 +1,6 @@
+
+function SecondComponent () {
+    return <h1 className="main-text">Hello world!!!</h1>
+}
+
+export default SecondComponent
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import FirstComponent from './components/FirstComponent/FirstComponent.jsx'
+import SecondComponent from './components/SecondComponent/SecondComponent.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <FirstComponent />
+    <SecondComponent />
   </React.StrictMode>,
 )
