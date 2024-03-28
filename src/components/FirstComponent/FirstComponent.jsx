74781aa9f51f436e41492831fbcc29a7822f3f66
Yaroslav <999jarik@gmail.com>
2024-03-28 23:57:02 +0200

    Initial commit

---
+++ src/components/FirstComponent/FirstComponent.jsx
@@ -0,0 +1,14 @@
+import reactLogo from './../../assets/react.svg'
+import './FirstComponent.css'
+
+
+function FirstComponent() {
+    return (
+        <>
+            <h1 className='title'>TEXT</h1>
+            <img src={reactLogo} className="logo react" alt="Logo" />
+        </>
+    )
+}
+
+export default FirstComponent
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FirstComponent from './components/FirstComponent/FirstComponent.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FirstComponent />
+  </React.StrictMode>,
+)
